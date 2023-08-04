0f79156aaf75a514939e0f42e2c09a23fe552de3
donnJik <superdumankhan@gmail.com>
2023-08-04 23:33:54 +0600

    5

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,18 @@ public class Main {
         linkedList.addNode(5);
         linkedList.addNode(5);
         linkedList.printList();
-        //System.out.println(linkedList.size());
-       // System.out.println(linkedList.get(4));
-       // linkedList.remove(3);
+        System.out.println(linkedList.size());
+        System.out.println(linkedList.get(4));
+        linkedList.remove(3);
         linkedList.printList();
 
         MyArrayList arrayList= new MyArrayList();
-        arrayList.addElement(40);
+        arrayList.addElement(40);//Добавляет элемент в массив.
         arrayList.addElement(44);
-        System.out.println(arrayList.size());
-        System.out.println(arrayList.get(0));
-        arrayList.delete(0);
-        System.out.println(arrayList.size());
-        System.out.println(arrayList.get(0));
+        System.out.println(arrayList.size());//Возвращает количество элементов.
+        System.out.println(arrayList.getElement(0));//Возвращает элемент под 0 индексом.
+        arrayList.delete(0);//Удаляет элемент под 0 индексом.
+        System.out.println(arrayList.size());//Возвращает количество элементов после удаления жлемента.
+        System.out.println(arrayList.getElement(0));//Возвращает элемент под индексом 0 после удаления.
     }
 }
